0638e0a9ffc66a1290495907610e2a721e1933f5
isaias-silva <zackblackmangold@gmail.com>
2021-08-31 12:36:41 -0300

    canvas pronto, animacoes e inteiraçoes dinamicas

--- js/script.js
+++ js/script.js
@@ -1,78 +1,98 @@
-const stands = [{
-    name: 'the world', user: 'dio brando', part: 3,
 
-    stats: {
+class Stand {
+    #id
+    #name
+    #user
+    #desc
+    #stats
+    #srci
+    #color
 
-        power: [150, 20],//a
-        speed: [250, 45],//a
-        range: [195, 85],//C
-        durability: [150, 130],//a
-        precision: [90, 95],//b
-        potential: [85, 55]//b
+    get color() {
+        return this.#color;
+
+    }
+    get id() {
+        return this.#id;
+    }
+
+    get srci() {
+        return this.#srci;
 
     }
 
-}]
+    get name() {
+        return this.#name;
+    }
 
-function setGraf() {
+    get user() { return this.#user }
+
+
+    get desc() {
+        return this.#desc;
+    }
+
+    get stats() {
+        return this.#stats
+    }
+
+
+
+    constructor(id, name, user, desc, stats, srci, c) {
+        this.#name = name;
+        this.#user = user;
+        this.#stats = stats;
+        this.#desc = desc;
+        this.#id = id;
+        this.#srci = srci;
+        this.#color = c;
+    }
+
+}
+
+
+
+
+const stands = [new Stand(1, 'zawarudo', 'dio brando', 'super strength stop time', {
+    power: [150, 35],//a
+    speed: [220, 55],//a
+    range: [195, 85],//C
+    durability: [150, 112],//a
+    precision: [100, 90],//b
+    potential: [100, 60]//b
+}, './imagens_teste/ZaWarudo.png', 'yellow'),
+
+new Stand(2, 'starplatinum', 'jotaro kujo', 'super strength and stop time', {
+    power: [150, 35],//a
+    speed: [220, 55],//a
+    range: [195, 85],//C
+    durability: [150, 112],//a
+    precision: [100, 90],//b
+    potential: [100, 60]//b
+}, './imagens_teste/starplatinum.png', 'blue')
+]
+
+
+
+
+function setGraf(ps) {
     const graf = document.getElementById("status");
 
     let ctx = graf.getContext("2d");
-    let p = stands[0].stats.potential;
-    let pr = stands[0].stats.precision;
-    let dur = stands[0].stats.durability;
-    let rag = stands[0].stats.range;
-    let vel = stands[0].stats.speed;
-    let power = stands[0].stats.power
-
-
-    //coordenadas E
-
-    //potencial(135,70)
-    //precisão(135,80)
-    //durabilidade(150,85)
-    //range(165,80)
-    //velocidade(165,70)
-    //poder(150,65)
-
-    //coordenadas D
-
-    //potencial(125,65)
-    //precisão(125,80)
-    //durabilidade(150,90)
-    //range(175,80)
-    //velocidade(175,65)
-    //poder(150,60)
-
-    //coordenadas C
-    //potencial(105,60)
-    //precisão(105,90)
-    //durabilidade(150,100)
-    //range(195,85)
-    //velocidade(190,65)
-    //poder(150,50)
-
-    //coordenadas B
-    //potencial(85,55)
-    //precisão(90,95)
-    //durabilidade(150,112)
-    //range(220,100)
-    //velocidade(220,55)
-    //poder(150,35)
-
-
-    //coordenadas A
-    //potencial(60,50)
-    //precisão(60,100)
-    //durabilidade(150,130)
-    //range(240,100)
-    //velocidade(250,45)
-    //poder(150,20)
+    let p = stands[ps].stats.potential;
+    let pr = stands[ps].stats.precision;
+    let dur = stands[ps].stats.durability;
+    let rag = stands[ps].stats.range;
+    let vel = stands[ps].stats.speed;
+    let power = stands[ps].stats.power
 
 
 
 
-    ctx.fillStyle = 'yellow';
+
+
+
+    ctx.fillStyle = stands[ps].color;
     ctx.beginPath();
 
 
@@ -101,15 +121,30 @@ function setGraf() {
 
     ctx.stroke();
 }
-function load() {
+function load(p) {
     let standuser = document.getElementById('user')
     let standname = document.getElementById('stand')
-    let part = document.getElementById('part')
-    part.innerHTML = stands[0].part;
-    standname.innerHTML = stands[0].name;
-    standuser.innerHTML = stands[0].user;
+    let desc = document.getElementById('desc')
+    let standcard = document.getElementById('standcard')
+    let section = document.getElementById('section')
+    let img = document.getElementById('img')
+    section.style.filter = "invert(100)"
+    section.style.backgroundSize = "60%"
+    standcard.style.filter = "opacity(0)"
+    setTimeout(loadenter, 1000)
 
-    setGraf()
+    function loadenter() {
+        section.style.backgroundSize = "70%"
+        section.style.filter = "invert(0)"
+        standcard.style.filter = "opacity(1)"
+        img.setAttribute('src', stands[p].srci);
+        desc.innerHTML = stands[p].desc;
+        standname.innerHTML = stands[p].name;
+        standuser.innerHTML = stands[p].user;
+
+        setGraf(p)
+    }
 
 }
-load();
+load(0);
+
